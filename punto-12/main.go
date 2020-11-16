6fd63d5035f0e695073d2af0ea078778b4ff7dae
Jaime Ramos <jrossetes@uninorte.edu.co>
2020-11-16 01:51:15 -0500

    fix: better code structure, validations and regexp working

--- punto-12/main.go
+++ punto-12/main.go
@@ -1,44 +1,96 @@
 package main
 
 import (
-	"fmt"
 	"os"
+	"fmt"
+	"bufio"
 	"regexp"
 	"strconv"
+	"strings"
 )
+
+//function that asks the user how many decimals is going to use the regexp
+func ask_precision_decimals() int {
+
+		fmt.Println("Digite el numero de decimales de precisión que deben tener los números:")
+
+		var n string
+	    _, err := fmt.Scanf("%s", &n)
+
+		match_int, _ := regexp.MatchString("^[0-9]+$", n)
+
+		if err != nil || !match_int {
+			fmt.Println("¡No digitó un entero correcto!")
+			return -1
+		}
+
+		n_int, _ := strconv.Atoi(n)
+		return n_int
+}
+
+//function that asks the users for the numbers to validate if meets the regexp rules
+func ask_numbers_to_test() []string {
+
+	fmt.Println("Digite los números a probar, separados por un espacio.")
+
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+  line := scanner.Text()
+
+	cases := strings.Fields(line)
+
+	return cases
+}
+
+//function that iterates over every number given by the user and determine if meet the pattern
+func eval_pattern(numbers []string, decimals int) {
+
+	colorYellow := "\033[33m"
+	colorReset := "\033[0m"
+
+	if decimals == 0 {
+
+		for _, s := range numbers {
+
+			match,_ := regexp.MatchString("^[0-9]+$", s)
+			if match {
+				fmt.Println(string(colorYellow), s, string(colorReset),"SI es un decimal de", decimals, "decimales de precisión")
+				continue
+			}
+
+			fmt.Println(string(colorYellow), s, string(colorReset), "NO es un decimal de", decimals, "decimales de precisión")
+
+		}
+
+	} else {
+
+		reg_exp := fmt.Sprintf(`^\-?[0-9]+\.[0-9]{%d}$`, decimals)
+
+		for _, s := range numbers {
+
+			match, _ := regexp.MatchString(reg_exp, s)
+
+			if match {
+				fmt.Println(string(colorYellow), s, string(colorReset), "SI es un decimal de", decimals, "decimales de precisión")
+				continue
+			}
+
+			fmt.Println(string(colorYellow), s, string(colorReset), "No es un decimal de", decimals, "decimales de precisión")
+
+		}
+
+	}
+
+}
 
 func main() {
 
-	// Se toma por comando el string a ingresar y el entero X correspondiente a la cantidad de decimales
-	args := os.Args
-	argsLength := len(args)
-
-	if argsLength <= 2 {
-		fmt.Println("¡Se necesita por lo menos un numero con n decimales para verificar!")
-
-	} else {
-		input := os.Args[1]
-		n := os.Args[2]
-
-		if i, err := strconv.Atoi(n); err == nil {
-
-			// Expresión regular
-			response := fmt.Sprintf(`^[0-9]+(?:\.\d{%d,%d})?$`, i, i)
-
-			re := regexp.MustCompile(response)
-
-			if re.FindString(input) == "" {
-				fmt.Printf("No es un número real o no tiene la cantidad exacta de %d decimales", i)
-			} else {
-				if i == 1 {
-					fmt.Printf(" %v Es un numero real con %d decimal", re.FindString(input), i)
-				} else {
-					fmt.Printf(" %v Es un numero real con %d decimales", re.FindString(input), i) // True
-				}
-
-			}
-
-		}
+	decimal_precision := ask_precision_decimals()
+	//only positive numbers accepted
+	if decimal_precision >= 0 {
+		array_numbers_to_test := ask_numbers_to_test()
+		eval_pattern(array_numbers_to_test, decimal_precision)
 	}
+
 
 }
